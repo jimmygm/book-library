21f3e58721257d2b4356ef2093e1c19b96b33397
Jimmy Garita <jimmy.garita.m@gmail.com>
2021-04-11 20:59:07 -0600

    Add basic functionality

--- script.js
+++ script.js
@@ -1,23 +1,46 @@
 let myLibrary = [];
+// Set up our database
+let database = firebase.database().ref('database');
+// Load our saved database
+database.on('value', (snapshot) => {
+    myLibrary = snapshot.val().library;
+    // Add Book.prototype to every book__proto__ (book.prototype didn't work)
+    myLibrary.forEach((book) => (book.__proto__ = Object.create(Book.prototype)));
+    updateLibrary();
+});
+// There are functions to update and delete,
+// but I prefer just to replace the whole object in one go
+function saveToCloud() {
+    database.set({
+        library: myLibrary,
+    });
+}
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, image) {
+    const defaultImage =
+        'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.431PLxi6NCvli-k5zlQ5uwHaLG%26pid%3DApi&f=1';
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
+    this.read = read ? 'Read' : 'Not read';
+    this.image = image == '' ? defaultImage : image;
 }
-
 Book.prototype.toggleRead = function () {
-    this.read = !this.read;
+    this.read = this.read == 'Read' ? 'Not read' : 'Read';
 };
 
 function addBookToLibrary() {
-    let title = prompt('What is the name of the book?');
-    let author = prompt('Who is the the author?');
-    let pages = prompt('How many pages it has?');
-    let read = confirm('Have you finished reading it?');
-    let newBook = new Book(title, author, pages, read);
+    hideBookInfoWindow();
+
+    let title = bookTitle.value;
+    let author = bookAuthor.value;
+    let pages = bookPages.value;
+    let read = bookRead.value;
+    let image = bookImage.value;
+    let newBook = new Book(title, author, pages, read, image);
     myLibrary.push(newBook);
+    // Save myLibrary to Firebase Database
+    saveToCloud();
     updateLibrary();
 }
 
@@ -30,12 +53,25 @@ function updateLibrary() {
         let newDiv = createCard(book, index);
         library.appendChild(newDiv);
     });
+
+    addBookButton();
+    resetBookInfo();
+}
+
+function addBookButton() {
+    let newButton = document.createElement('button');
+    newButton.textContent = '+';
+    newButton.setAttribute('id', 'add-book');
+    newButton.onclick = showBookInfoWindow;
+    library.appendChild(newButton);
 }
 
 function createCard(book, idx) {
     let bookCard = addDiv(idx);
-    addSpans(book, bookCard);
     addRemoveButton(bookCard);
+    addImage(book, bookCard);
+    addSpans(book, bookCard);
+    addReadToggle(book, bookCard);
     return bookCard;
 }
 
@@ -46,18 +82,35 @@ function addDiv(idx) {
     return newDiv;
 }
 
-function addSpans(book, parent) {
-    let bookProperties = Object.keys(book);
+function addImage(book, parent) {
+    let newImage = document.createElement('img');
+    newImage.setAttribute('src', book.image);
+    parent.appendChild(newImage);
+}
 
-    bookProperties.forEach((property) => {
-        let newSpan = document.createElement('span');
-        newSpan.textContent = book[property];
-        parent.appendChild(newSpan);
-    });
+function addSpans(book, parent) {
+    let spanTitle = document.createElement('span');
+    spanTitle.textContent = `Title: ${book.title}`;
+
+    let spanAuthor = document.createElement('span');
+    spanAuthor.textContent = `Author: ${book.author}`;
+
+    let spanPages = document.createElement('span');
+    spanPages.textContent = book.pages;
+
+    let spanRead = document.createElement('span');
+    spanRead.textContent = book.read;
+
+    parent.appendChild(spanTitle);
+    parent.appendChild(spanAuthor);
+    parent.appendChild(spanPages);
+    parent.appendChild(spanRead);
 }
 
 function addRemoveButton(bookCard) {
     let removeButton = document.createElement('button');
+    removeButton.textContent = 'X';
+    removeButton.classList.add('remove-btn');
     removeButton.addEventListener('click', removeBookCard);
     bookCard.appendChild(removeButton);
 }
@@ -65,11 +118,48 @@ function addRemoveButton(bookCard) {
 function removeBookCard(e) {
     let bookIndex = e.target.parentNode.getAttribute('data-index');
     myLibrary.splice(bookIndex, 1);
+    saveToCloud();
     updateLibrary();
 }
 
+function addReadToggle(book, parent) {
+    let toggleInput = document.createElement('input');
+    toggleInput.setAttribute('type', 'checkbox');
+    if (book.read == 'Read') toggleInput.checked = true;
+    toggleInput.addEventListener('click', (e) => {
+        book.toggleRead();
+        updateLibrary();
+    });
+
+    parent.appendChild(toggleInput);
+}
+
+function showBookInfoWindow() {
+    bookInfoWindow.style['display'] = 'grid';
+}
+function hideBookInfoWindow() {
+    bookInfoWindow.style['display'] = 'none';
+}
+
+function resetBookInfo() {
+    bookTitle.value = '';
+    bookAuthor.value = '';
+    bookPages.value = '';
+    bookRead.value = '';
+    bookImage.value = '';
+}
+
 const library = document.querySelector('#library');
-const addBookButton = document.querySelector('#add-book');
+const bookInfoWindow = document.querySelector('#book-info');
+const bookTitle = document.querySelector('#book-title');
+const bookAuthor = document.querySelector('#book-author');
+const bookPages = document.querySelector('#book-pages');
+const bookRead = document.querySelector('#book-read');
+const bookImage = document.querySelector('#book-image');
+const bookCancel = document.querySelector('#book-cancel');
+const bookConfirm = document.querySelector('#book-confirm');
 
 updateLibrary();
-addBookButton.addEventListener('click', addBookToLibrary);
+addBookButton.onclick = showBookInfoWindow;
+bookCancel.onclick = hideBookInfoWindow;
+bookConfirm.onclick = addBookToLibrary;
